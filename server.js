654b3733653646bacf678b5011362334ea3b7fb0
Shane Keulen <shane.keulen@gmail.com>
2015-07-10 16:52:50 -0400

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import upcase from './routeHandlers/upcase';
 import reverse from './routeHandlers/reverse';
 import upcaseReverse from './routeHandlers/upcaseReverse';
 
 
 const app = express();
+app.use(cors());
 
 app.get('/upcase', upcase);
 app.get('/reverse', reverse);
